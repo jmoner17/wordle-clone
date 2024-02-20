5e44cbb24ced4e2b0ea162f18387b7c1447f9eba
Izaak White <ww655421@ohio.edu>
2024-02-20 11:09:35 -0500

    hamburger

--- src/app/(pages)/example-folder/page.js
+++ src/app/(pages)/example-folder/page.js
@@ -3,12 +3,23 @@
 
 export default function Home () {
 
-
+  //let theText = myTextInput.value;
 
   //this is where you will put html function javascript stuff goes above
   return (
     <div>
       <h1>Hello, Next.js!</h1>
+        <label for="name">Name (4 to 8 characters):</label>
+        <label for="uname">Choose a username: </label>
+         <input
+          type="text"
+          id="uname"
+          name="name"
+          placeholder="Lower case, all one word" />
+        <div>
+           <button>Submit</button>
+       </div>
     </div>
+    
   );
 };

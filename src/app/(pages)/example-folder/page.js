f16d4ea9e142b90cc3e581f17729ffcfda103174
Izaak White <ww655421@ohio.edu>
2024-02-20 11:49:27 -0500

    initial commit

--- src/app/(pages)/example-folder/page.js
+++ src/app/(pages)/example-folder/page.js
@@ -1,23 +1,23 @@
+'use client'
 import React, { useEffect, useState, useRef } from "react";
 import { useSupabase } from "@/utils/supabase-provider";
 
 export default function Home () {
 
+  const[num, setNum] = useState(0);
+
   //let theText = myTextInput.value;
+  useEffect(() => {
+    
+  }, [])
 
   //this is where you will put html function javascript stuff goes above
   return (
     <div>
       <h1>Hello, Next.js!</h1>
-        <label for="name">Name (4 to 8 characters):</label>
-        <label for="uname">Choose a username: </label>
-         <input
-          type="text"
-          id="uname"
-          name="name"
-          placeholder="Lower case, all one word" />
+        
         <div>
-           <button>Submit</button>
+           <button onClick={} style={{marginTop: '180px', padding: '240px 240px', fontSize: '20px', cursor: 'pointer'}}>{int}</button>
        </div>
     </div>
     

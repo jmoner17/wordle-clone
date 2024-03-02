36fb660e36eab47e9f5f2b2c4d13f848c8bc9568
Joshua Moner <jm170321@ohio.edu>
2024-03-02 00:50:07 -0500

    added python script to simulate slot machine payouts

--- src/app/(pages)/gamba/ClientRendering.js
+++ src/app/(pages)/gamba/ClientRendering.js
@@ -3,55 +3,36 @@
 import React, { useEffect, useRef } from "react";
 import LetterBox from "@/components/LetterBox"; // Adjust the import path if necessary
 
-const ClientComponent = () => {
-    const visibleContainerRef = useRef(null); // Reference to the container of visible LetterBoxes
+const ClientComponent = ({children}) => {
 
-    // Create an array with six empty letters to represent the six visible LetterBoxes
-    const visibleLetters = Array(6).fill('');
+    const ROW_SIZE = 6;
+    const LETTER_SIZE = 5;
 
-    // Create an additional array for the extra LetterBoxes
-    const extraLetters = Array(10).fill('');
 
-    useEffect(() => {
-        // Function to update based on the visible container's height
-        const updateVisibleContainerHeight = () => {
-            if (visibleContainerRef.current) {
-                const height = visibleContainerRef.current.offsetHeight;
-                console.log("Visible Container Height:", height); // For demonstration
-            }
-        };
-
-        updateVisibleContainerHeight();
-
-        // Add event listener for window resize
-        window.addEventListener('resize', updateVisibleContainerHeight);
-
-        // Cleanup listener on unmount
-        return () => {
-            window.removeEventListener('resize', updateVisibleContainerHeight);
-        };
-    }, []);
+    
 
     return (
-        <div className="flex justify-center items-center h-screen">
-            <div
-                className="flex flex-col items-center absolute overflow-visible"
-                style={{ top: '20vh' }} // This positions the top of the container at 1/3 of the viewport height
-            >
-                {/* Main container for Visible LetterBoxes, centered on the screen */}
-                <div className="button-shrink flex flex-col" ref={visibleContainerRef}>
-                    {visibleLetters.map((letter, index) => (
-                        <LetterBox key={index} letter={letter} feedback="" />
-                    ))}
-                </div>
-                {/* Extra LetterBoxes container positioned directly below the main container */}
-                <div className="flex flex-col">
-                    {extraLetters.map((letter, index) => (
-                        <LetterBox key={`extra-${index}`} letter={letter} feedback="" />
-                    ))}
-                </div>
+        <main className="gradient-background flex flex-col items-center absolute inset-0 justify-center overflow-auto">
+            <div>{children}</div>
+            <div className="flex-grow-0 w-full justify-center">
+                {letter.map((row, rowIndex) => (
+                    // biome-ignore lint/suspicious/noArrayIndexKey: <needed for wordle functionality>
+                    <div key={rowIndex} className="flex items-center justify-center space-x-4 my-1">
+                        {row.map((letter, letterIndex) => (
+                            <LetterBox
+                                // biome-ignore lint/suspicious/noArrayIndexKey: <needed for wordle functionality>
+                                key={letterIndex}
+                                ref={refRow.current[rowIndex][letterIndex]}
+                                letter={letter}
+                                error={error}
+                                index={letterIndex}
+                                feedback={feedback[rowIndex][letterIndex]}
+                            />
+                        ))}
+                    </div>
+                ))}
             </div>
-        </div>
+        </main>
     );
 };
 
